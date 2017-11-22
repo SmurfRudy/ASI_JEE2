fc51b67d9497ccd2ae11138d18ccf1f2e9cb93e8
RudyDeal <rudy.deal.rd@gmail.com>
2017-11-22 18:03:15 +0100

    valide avec le prof besoin de finir pour le 10 decembre

--- asi2-JEE/DB/CreationBDD_TPJEE.sql
+++ asi2-JEE/DB/CreationBDD_TPJEE.sql
@@ -3,7 +3,7 @@ CREATE DATABASE LocalDB;
 
 USE LocalDB;
 DROP TABLE IF EXISTS T_USERS;
-CREATE TABLE T_USERS (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,login CHAR(25) NOT NULL, password CHAR(25) NOT NULL, first_name CHAR(25), last_name CHAR(25), birthdate DATE);
-INSERT INTO T_USERS (login, password, first_name, last_name, birthdate) VALUES ("Tintin", "Milou", "Prosper", "Hergé", '1914-12-25');
-INSERT INTO T_USERS (login, password, first_name, last_name, birthdate) VALUES ("Asterix", "Idefix", "René", "Goscini", '1948-01-25');
-INSERT INTO T_USERS (login, password, first_name, last_name, birthdate) VALUES ("Mickey", "Pluto", "Walt", "Disney", '1992-10-14');
+CREATE TABLE T_USERS (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,login varchar(25) NOT NULL, password varchar(25) NOT NULL, first_name varchar(25), last_name varchar(25),role varchar(25), birthdate DATE);
+INSERT INTO T_USERS (login, password, first_name, last_name, role, birthdate) VALUES ("Tintin", "Milou", "Prosper", "Hergé", "ADMIN", '1914-12-25');
+INSERT INTO T_USERS (login, password, first_name, last_name, role, birthdate) VALUES ("Asterix", "Idefix", "René", "Goscini", "USER", '1948-01-25');
+INSERT INTO T_USERS (login, password, first_name, last_name, role, birthdate) VALUES ("Mickey", "Pluto", "Walt", "Disney", "ADMIN", '1992-10-14');
--- asi2-JEE/Projet/FrontAuthWatcherWebService/src/main/java/fr/cpe/rest/IUserRestService.java
+++ asi2-JEE/Projet/FrontAuthWatcherWebService/src/main/java/fr/cpe/rest/IUserRestService.java
@@ -3,39 +3,27 @@ package fr.cpe.rest;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import fr.cpe.model.User;
 
 @Path("/WatcherAuth")
+
 public interface IUserRestService {
 
-	/*@POST
+	
+	@GET
+	@Produces("text/plain")
+	@Path("/test")
+	String getTest();
+	
+	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/")
-	void addUser(User user);
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/")
-	List<User> listUser();
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/{id}")
-	User getUser(@PathParam(value = "id") Integer id);
-	*/
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/{login,pwd}")
-	User getUser(@PathParam(value = "login") String login,
-			@PathParam(value = "pwd") String pwd);
+	Response getUser(User user);
 }
